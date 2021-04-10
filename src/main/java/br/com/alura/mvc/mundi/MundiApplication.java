309fc3eac87ecb4d266534bfcf8076f12ab04377
Edson Batista - Smiles <edson.batista@smiles.com.br>
2021-04-10 15:02:10 -0300

    teste commit

--- src/main/java/br/com/alura/mvc/mundi/MundiApplication.java
+++ src/main/java/br/com/alura/mvc/mundi/MundiApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MundiApplication {
 
+	// classe
 	public static void main(String[] args) {
 		SpringApplication.run(MundiApplication.class, args);
 	}
-
 
 }
